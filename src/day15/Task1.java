43b36ed0cecd74e0dad4fc01dfadc7e4a0de7793
Karasev Roman <progressivenation12@gmail.com>
2023-05-23 22:31:31 +0300

    Change in the folder hierarchy, passed the 15th day of the marathon!

---
+++ src/day15/Task1.java
@@ -0,0 +1,27 @@
+package java.day15;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File fileRead = new File("src/main/resources/shoes.csv");
+        Scanner scanner = new Scanner(fileRead);
+
+        File fileWrite = new File("src/main/resources/missing_shoes.txt");
+        PrintWriter printWriter = new PrintWriter(fileWrite);
+
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(";");
+            if (Integer.parseInt(line[2]) == 0) {
+                printWriter.printf("%-40s %3sр., %sшт.\n", line[0], line[1], line[2]);
+            }
+        }
+
+        printWriter.close();
+        scanner.close();
+    }
+}
