7de0bdfdbbbee0e002013a5019e2392216aa8afe
Karasev Roman <progressivenation12@gmail.com>
2023-05-23 22:36:20 +0300

    Update

--- src/day15/Task1.java
+++ src/day15/Task1.java
@@ -1,4 +1,4 @@
-package java.day15;
+package day15;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,20 +7,18 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) throws FileNotFoundException {
-
-        File fileRead = new File("src/main/resources/shoes.csv");
+        File fileRead = new File("resources/shoes.csv");
         Scanner scanner = new Scanner(fileRead);
 
-        File fileWrite = new File("src/main/resources/missing_shoes.txt");
+        File fileWrite = new File("resources/missing_shoes.txt");
         PrintWriter printWriter = new PrintWriter(fileWrite);
 
         while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split(";");
             if (Integer.parseInt(line[2]) == 0) {
-                printWriter.printf("%-40s %3sр., %sшт.\n", line[0], line[1], line[2]);
+                printWriter.printf("%-40s %3s р-р., %sшт.\n", line[0], line[1], line[2]);
             }
         }
-
         printWriter.close();
         scanner.close();
     }
